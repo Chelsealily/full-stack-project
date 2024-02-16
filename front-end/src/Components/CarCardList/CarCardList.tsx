aeb441e60d5b0d1f6f4939fcb6b6e30eede132a7
Chelsea Li <148104000+Chelsealily@users.noreply.github.com>
2024-02-16 10:10:36 +0000

    deleted line

--- front-end/src/Components/CarCardList/CarCardList.tsx
+++ front-end/src/Components/CarCardList/CarCardList.tsx
@@ -7,8 +7,6 @@ type CarCardListProps ={
   cars:CarType[];
 }
 
-//later on I need to add the edit link for each card
-
 const CarCardList = ({cars}:CarCardListProps) => { 
   return ( 
     <div className="card-list">
